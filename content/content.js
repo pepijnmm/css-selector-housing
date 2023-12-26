37c2d20f6e39d521d5edb2521398b9928b5e4e07
pepijn <pepijn.mommersteeg@gmail.com>
2023-12-26 23:58:19 +0100

    working example
    problem is that it won't create a path with the whitelist attributes

--- content/content.js
+++ content/content.js
@@ -1,17 +1,27 @@
 //variables
 let previousElement;
+let border;
 //listener
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	if (message.option == "clickAction") {
+		if (border != null) {
+			stopTracking();
+		}
+		createborder();
 		startTracking();
 	}
 });
 function startTracking() {
 	document.addEventListener("mousemove", hoverElement, { passive: true });
 	document.addEventListener("click", clickElement, { passive: false });
+	border.classList.add("border");
 }
 function hoverElement(event) {
+	//hide border so that we can see what the use is standing on
+	//put border back so if he clicks he won't click on a element that goes to the next page or submits something
+	border.classList.remove("border");
 	let element = document.elementFromPoint(event.clientX, event.clientY);
+	border.classList.add("border");
 	if (element != null && element != previousElement) {
 		if (previousElement != null) previousElement.classList.remove("css-selector-active");
 		element.classList.add("css-selector-active");
@@ -20,13 +30,26 @@ function hoverElement(event) {
 }
 function clickElement(event) {
 	event.preventDefault();
-	let element = document.elementFromPoint(event.clientX, event.clientY);
-	previousElement.classList.remove("css-selector-active");
-	if (element != null) {
+	if (previousElement != null) {
 		stopTracking();
-		console.log(element);
+		console.log(
+			CssSelectorGenerator.getCssSelector(previousElement, {
+				selectors: ["test-id", "class", "tag", "id", "attribute"],
+				whitelist: ["test-id", "attribute"],
+			})
+		);
 	}
 }
 function stopTracking() {
 	document.removeEventListener("mousemove", hoverElement, { passive: true });
+	document.removeEventListener("click", clickElement, { passive: false });
+	previousElement.classList.remove("css-selector-active");
+	border.classList.remove("border");
+	border = null;
+}
+
+function createborder() {
+	border = document.createElement("div");
+	document.body.appendChild(border);
+	border.classList.add("bordertwo");
 }
